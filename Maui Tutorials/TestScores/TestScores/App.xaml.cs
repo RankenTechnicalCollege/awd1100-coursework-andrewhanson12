8c3f009a17f0fbd4fd039151455bfdffd778c5a5
andrewhanson12 <andrew_hanson@insideranken.org>
2025-06-08 10:06:24 -0500

    Commit Maui Test Score App

---
+++ Maui Tutorials/TestScores/TestScores/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace TestScores
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "My Score Calculator";
+            return window;
+        }
+    }
+}
---
+++ Maui Tutorials/TestScores/TestScores/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace TestScores
+{
+    public partial class MainPage : ContentPage
+    {
+        int totalScore = 0;
+        int testScoreCount = 0;
+        float averageScore = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddScoreClicked(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtScoreEntry.Text, out int testScore) && testScore >= 0 && testScore <= 100)
+            {
+                ++testScoreCount;
+
+                totalScore = totalScore + testScore;
+                averageScore = (float)totalScore / testScoreCount;
+
+                lblTotalScore.Text = $" {totalScore}";
+                lblScoreCount.Text = $" {testScoreCount}";
+                lblAverage.Text = $" {averageScore}";
+            }
+            else
+            {
+                DisplayAlert("Invalid Input", "Please enter a valid test score.", "OK");
+            }
+            txtScoreEntry.Text = string.Empty;
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            lblTotalScore.Text = string.Empty;
+            lblScoreCount.Text = string.Empty;
+            lblAverage.Text = string.Empty;
+            txtScoreEntry.Text = string .Empty;
+            totalScore = 0;
+            testScoreCount = 0;
+            averageScore = 0;
+        }
+
+        private void OnExitClicked(object sender, EventArgs e)
+        {
+            Application.Current.Quit();
+        }
+    }
+}
